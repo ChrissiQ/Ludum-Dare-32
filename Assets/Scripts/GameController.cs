cb7146bf5d05eac64ed158a97ff12c8f6df1b021
ChrissiQ <rediahs@gmail.com>
2015-04-20 19:09:37 -0600

    Added panic audio, enabled panic audio when horse is heading towards you, reduced number of foliage so the game is over in a reasonable amount of time.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,24 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 	
 	public int foliageCount;
 	public List<GameObject> foliage;
 	public GameObject foliageContainer;
-	public float xsize = 50f;
-	public float zsize = 50f;
+	public float xsize = 25f;
+	public float zsize = 25f;
+	public Text gameOver;
+	bool dead;
 
 	public GameObject GetRandomFoliage () {
-		return foliageContainer.transform.GetChild (Random.Range (0, foliageContainer.transform.childCount-1)).gameObject;
+		return foliageContainer.transform.GetChild (Random.Range (0, foliageContainer.transform.childCount)).gameObject;
+	}
+
+	public void Die() {
+		dead = true;
 	}
 
 	void InstantiateRandom() {
 		float x = Random.Range (0-xsize, xsize);
 		float z = Random.Range (0-xsize, zsize);
+
 		GameObject gameObject = (GameObject) Instantiate (
-			foliage[Random.Range (0, foliage.Count-1)],
+			foliage[Random.Range (0, foliage.Count)],
 			new Vector3 (x, 0f, z),
 			Quaternion.identity);
 		gameObject.transform.parent = foliageContainer.transform;
@@ -37,5 +45,11 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void FixedUpdate() {
+		if (dead) {
+			gameOver.enabled = true;
+		}
 	}
 }
--- Assets/Scripts/HorseMovement.cs
+++ Assets/Scripts/HorseMovement.cs
@@ -2,28 +2,52 @@
 using System.Collections;
 
 public class HorseMovement : MonoBehaviour {
+	
+	public GameController gameController;
+	public float waitBetweenTargets;
+	public AudioSource panicMusic;
 
 	Animator anim;
 	Rigidbody rb;
 	GameObject target;
-	public GameController gameController;
+	float lastEaten;
+	AudioSource backgroundMusic;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent <Animator> ();
-		anim.SetTrigger ("Walk");
 		rb = GetComponent <Rigidbody> ();
+		lastEaten = Time.time;
+		backgroundMusic = gameController.GetComponent <AudioSource> ();
+		backgroundMusic.Play();
 	}
 
 	void FixedUpdate () {
-		if (target == null) {
+
+		if (Time.time - lastEaten < waitBetweenTargets) {
+			anim.SetBool("isWalking", false);
+
+		} else if (target == null) {
 			target = gameController.GetRandomFoliage();
 			rb.transform.LookAt(target.transform);
+			anim.SetBool("isWalking", true);
+			if (target.CompareTag("MainCharacter")) {
+				backgroundMusic.Stop();
+				panicMusic.Play();
+			} else {
+				backgroundMusic.Play();
+				panicMusic.Stop();
+			}
 		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject == target.gameObject)
+		if (target != null && other.gameObject == target.gameObject) {
+			lastEaten = Time.time;
+			if (other.CompareTag("MainCharacter")) {
+				gameController.Die();
+			}
 			Destroy(other.gameObject);
+		}
 	}
 }
